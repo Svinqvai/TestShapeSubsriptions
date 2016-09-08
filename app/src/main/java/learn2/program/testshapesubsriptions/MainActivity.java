1e1d6f5bc775501316392c4ae9f8d79d84d5ede0
ando <radko_pic@abv.bg>
2016-09-08 17:10:21 +0300

    ogun

--- app/src/main/java/learn2/program/testshapesubsriptions/MainActivity.java
+++ app/src/main/java/learn2/program/testshapesubsriptions/MainActivity.java
@@ -1,20 +1,24 @@
 package learn2.program.testshapesubsriptions;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 import learn2.program.testshapesubsriptions.billing_util.IabHelper;
 import learn2.program.testshapesubsriptions.billing_util.IabResult;
-
+import learn2.program.testshapesubsriptions.billing_util.Inventory;
+import learn2.program.testshapesubsriptions.billing_util.Purchase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,10 +32,17 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView pearImgView;
 
-    private int itemClicked;
+    public static int subscriptionIndex = -1;
 
 
-    private static final int RC_REQUEST = 10001;
+    private String currentSubscriptionPeriod;
+
+    private boolean hasSubscription;
+
+    private boolean mAutoRenewEnabled;
+
+
+    private static final int RC_REQUEST = 10023;
     private IabHelper mHelper;
     String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvgtPCE24JN71QUFgdFde8Zu7mop3ozX4SWP3xd8s8xf1Dqov/0MUg957NcfAhMWkggFMn0DTByPV9lS68/tfrIPgkX96VQz9jluvH++kGfpkfQV3WfGbV7dGHNwD9gqMuZRy7V3efZZXrc1ewdut6MZE97i5lpo5lp3HVyfjnCl3yLmI/Di6l+UdKU+ZERkWOT1ONeww+lP71gz0UweGegyfbXDmYso33HW7bJcpjLmv9x3yhrbEqNXgalNFA1TQtS4sdh1O20m36xoKp4q1E35QPnM7Hg5/8qDPOF5k31OQHogigjRTSWTmiYVN5ynXM70viFBX65g0SNYtHh/emwIDAQAB";
 
@@ -50,63 +61,165 @@ public class MainActivity extends AppCompatActivity {
         oneYearBtn = (Button) findViewById(R.id.oneYearBtn);
         pearImgView = (ImageView) findViewById(R.id.pearImgView);
 
-
         oneMonthBtn.setOnClickListener(subscriptionCL);
         threeMonthsBtn.setOnClickListener(subscriptionCL);
         sixMonthsBtn.setOnClickListener(subscriptionCL);
         oneYearBtn.setOnClickListener(subscriptionCL);
-        pearImgView.setOnClickListener(imageViewCL);
 
         mHelper = new IabHelper(this, base64EncodedPublicKey);
+
         mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
             public void onIabSetupFinished(IabResult result) {
                 Log.d(Constants.APP_TAG, "IabHelper.OnIabSetupFinishedListener() --> Setup finished.");
 
                 if (!result.isSuccess()) {
-                    Log.d(Constants.APP_TAG, "Problem setting up in-app billing: " + result);
+                    complain("Problem setting up in-app billing: " + result);
                     return;
-                } else {
-                    Log.d(Constants.APP_TAG, "SUCCESS: " + result);
                 }
-
                 if (mHelper == null) {
                     return;
                 }
-                mHelper.queryInventoryAsync(true, subscriptionsKeys, new SubscriptionOnQueryInventoryFinished(subscriptionsKeys));
-                if (SubscriptionOnQueryInventoryFinished.hasSubscription) {
-                    pearImgView.setVisibility(View.VISIBLE);
+                try {
+                    mHelper.queryInventoryAsync(mGotInventoryListener);
+                } catch (IabHelper.IabAsyncInProgressException e) {
+                    complain("Error querying inventory. Another async operation in progress.");
                 }
             }
         });
+
+        if (!hasSubscription || !mAutoRenewEnabled) {
+            //Display all buttons
+        } else {
+            if (currentSubscriptionPeriod.equalsIgnoreCase(subscriptionsKeys.get(0))) {
+                oneMonthBtn.setEnabled(false);
+            }
+            if (currentSubscriptionPeriod.equalsIgnoreCase(subscriptionsKeys.get(0))) {
+                oneMonthBtn.setEnabled(false);
+                threeMonthsBtn.setEnabled(false);
+            }
+        }
     }
+
+
+    IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
+        public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
+
+            if (mHelper == null) {
+                return;
+            }
+
+            if (result.isFailure()) {
+                complain("Failed to query inventory: " + result);
+                return;
+            }
+            Log.d(Constants.APP_TAG, "Query inventory was successful.");
+
+            // First find out which subscription is auto renewing
+            for (String subscriptionKey : subscriptionsKeys) {
+                Purchase purchase = inventory.getPurchase(subscriptionKey);
+                if (purchase != null && purchase.isAutoRenewing()) {
+                    currentSubscriptionPeriod = subscriptionKey;
+                    mAutoRenewEnabled = true;
+                } else {
+                    currentSubscriptionPeriod = "";
+                    mAutoRenewEnabled = false;
+                }
+
+                if (purchase != null && verifyDeveloperPayload(purchase)) {
+                    hasSubscription = true;
+                }
+            }
+        }
+    };
+    //TODO on start decide which buttons to show based on current subscription
 
 
     private final View.OnClickListener subscriptionCL = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-
+            final String mSelectedSubscriptionPeriod;
             if (view == oneMonthBtn) {
-                // Toast.makeText(MainActivity.this, "One Month", Toast.LENGTH_SHORT).show();
-                itemClicked = 0;
+                mSelectedSubscriptionPeriod = subscriptionsKeys.get(0);
             } else if (view == threeMonthsBtn) {
-                //   Toast.makeText(MainActivity.this, "Three Months", Toast.LENGTH_SHORT).show();
-                itemClicked = 1;
+                mSelectedSubscriptionPeriod = subscriptionsKeys.get(1);
             } else if (view == sixMonthsBtn) {
-                //      Toast.makeText(MainActivity.this, "Six Months", Toast.LENGTH_SHORT).show();
-                itemClicked = 2;
+                mSelectedSubscriptionPeriod = subscriptionsKeys.get(2);
             } else {
-                //      Toast.makeText(MainActivity.this, "One Year", Toast.LENGTH_SHORT).show();
-                itemClicked = 3;
+                mSelectedSubscriptionPeriod = subscriptionsKeys.get(3);
             }
-            buy();
+
+            //TODO  Generate payload
+            //TODO Construct the key from some strings at runtime
+            //TODO amend the library
+            // Payload is additional argument  =that you want google play  to pass back along with purchase information
+            // product id = sku they are used to start purchase flow and other requests
+            //getPurchases return all the active subscriptions
+
+            //To upgrade or downgrade subscription I need to call getBuyIntentToReplaceSkus(). !!!!!!!!!!!!!!!!!!!!!!!!
+            //his method is passed the new SKU the user wants to buy, and all the old SKUs that are superseded by it.
+            //https://developer.android.com/google/play/billing/billing_subscriptions.html
+
+
+
+            //https://www.youtube.com/watch?v=mnA0gaQWtAM&list=PLOU2XLYxmsIJ6j7lT1xoqANWEJd-YbZqI&index=7
+            String payload = "";
+            List<String> oldSkus = null;
+            if (!TextUtils.isEmpty(currentSubscriptionPeriod)) {
+                // The user currently has a valid subscription, any purchase action is going to
+                // replace that subscription
+                oldSkus = new ArrayList<>();
+                oldSkus.add(currentSubscriptionPeriod);
+            }
+
+
+            try {
+                mHelper.launchPurchaseFlow(MainActivity.this, mSelectedSubscriptionPeriod, IabHelper.ITEM_TYPE_SUBS,
+                        oldSkus, RC_REQUEST, mPurchaseFinishedListener, payload);
+            } catch (IabHelper.IabAsyncInProgressException e) {
+                complain("Error launching purchase flow. Another async operation in progress.");
+            }
         }
     };
 
-    private final View.OnClickListener imageViewCL = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
+
+    IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
+            Log.d(Constants.APP_TAG, "Purchase finished: " + result + ", purchase: " + purchase);
+
+            if (mHelper == null) {
+                return;
+            }
+
+            if (result.isFailure()) {
+                complain("Error purchasing: " + result);
+                return;
+            }
+            if (!verifyDeveloperPayload(purchase)) {
+                complain("Error purchasing. Authenticity verification failed.");
+                return;
+            }
+            Log.d(Constants.APP_TAG, "Purchase successful.");
+
+
+            if (purchase != null) {
+                for (String subscriptionKey : subscriptionsKeys) {
+                    if (purchase.getSku().equalsIgnoreCase(subscriptionKey)) {
+                        Log.d(Constants.APP_TAG, "Subscription purchased.");
+                        alert("Thank you for subscription");
+                        hasSubscription = true;
+                        mAutoRenewEnabled = purchase.isAutoRenewing();
+                        currentSubscriptionPeriod = purchase.getSku();
+                    }
+                }
+            }
         }
     };
+
+
+    boolean verifyDeveloperPayload(Purchase purchase) {
+        String payload = purchase.getDeveloperPayload();
+        return true;
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -115,21 +228,36 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == RC_REQUEST && resultCode == RESULT_OK) {
             if (mHelper != null && !mHelper.handleActivityResult(requestCode, resultCode, data)) {
                 super.onActivityResult(requestCode, resultCode, data);
-
             }
         }
     }
 
+    void complain(String message) {
+        Log.e(Constants.APP_TAG, "**** TrivialDrive Error: " + message);
+        alert("Error: " + message);
+    }
 
-    private void buy() {
-        if (mHelper != null && mHelper.subscriptionsSupported()) {
-            mHelper.launchSubscriptionPurchaseFlow(MainActivity.this, subscriptionsKeys.get(itemClicked), RC_REQUEST, new SubscriptionPurchaseListener(subscriptionsKeys, mHelper, itemClicked), generateTocken());
-        }
+    void alert(String message) {
+        AlertDialog.Builder bld = new AlertDialog.Builder(this);
+        bld.setMessage(message);
+        bld.setNeutralButton("OK", null);
+        Log.d(Constants.APP_TAG, "Showing alert dialog: " + message);
+        bld.create().show();
     }
 
     private String generateTocken() {
         String token = "token-";
         Random random = new Random();
         return token + random.nextInt(99999);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(Constants.APP_TAG, "Destroying helper.");
+        if (mHelper != null) {
+            mHelper.disposeWhenFinished();
+            mHelper = null;
+        }
     }
 }
---
+++ app/src/main/java/learn2/program/testshapesubsriptions/QueryInventoryFinishedListener.java
@@ -0,0 +1,51 @@
+package learn2.program.testshapesubsriptions;
+
+import android.util.Log;
+
+import java.util.List;
+
+import learn2.program.testshapesubsriptions.billing_util.IabHelper;
+import learn2.program.testshapesubsriptions.billing_util.IabResult;
+import learn2.program.testshapesubsriptions.billing_util.Inventory;
+import learn2.program.testshapesubsriptions.billing_util.Purchase;
+
+
+public class QueryInventoryFinishedListener implements IabHelper.QueryInventoryFinishedListener {
+
+    private final List<String> subscriptions;
+
+    public QueryInventoryFinishedListener(List<String> subscriptions) {
+        this.subscriptions = subscriptions;
+    }
+
+
+    @Override
+    public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
+        if (result.isFailure()) {
+            Log.d(Constants.APP_TAG, "QueryForSubscriptionDetailsListener FAIL Cannot get the list of the subscriptions " + result);
+            return;
+        }
+
+        if (inventory != null) {
+            for (int i = 0; i < subscriptions.size(); i++) {
+                String subscriptionKey = subscriptions.get(i);
+
+                if (inventory.hasDetails(subscriptionKey)) {
+                    Log.d(Constants.APP_TAG, "Scu: " + inventory.getSkuDetails(subscriptionKey).getSku());
+                    Log.d(Constants.APP_TAG, "Scu Title: " + inventory.getSkuDetails(subscriptionKey).getTitle());
+                    Log.d(Constants.APP_TAG, "Scu Type: " + inventory.getSkuDetails(subscriptionKey).getType());
+                    Log.d(Constants.APP_TAG, "Scu Price: " + inventory.getSkuDetails(subscriptionKey).getPrice());
+                    Log.d(Constants.APP_TAG, "Scu Desc: " + inventory.getSkuDetails(subscriptionKey).getDescription());
+                    Log.d(Constants.APP_TAG, "Purchase status: " + (inventory.hasPurchase(subscriptionKey) ? "YES" : "NO"));
+
+                    Log.d(Constants.APP_TAG, "-------------------------------------------------------");
+
+                    if (inventory.hasPurchase(subscriptionKey)) {
+                        MainActivity.subscriptionIndex = i;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
